f569a9fe44cce57c2625431514cf0a12b9639a25
Elida Ayala Guerrero <ayala2160@gmail.com>
2022-02-14 16:28:30 -0600

    Pantalla de login

--- resources/views/connect/login.blade.php
+++ resources/views/connect/login.blade.php
@@ -1,6 +1,26 @@
 @extends('connect.master')
 
 @section('title', 'Login')
-    
+
 @section('content')
+    <div class="box box_login shadow">
+        {!! Form::open(['url' => '/login']) !!}
+        <label for="email">Correo electrónico: </label>
+        <div class="input-group">
+            <div class="input-group-prepend">
+                <div class="input-group-text"><i class="fa-solid fa-envelope-open-text"></i></div>
+            </div>
+            {!! Form::email('email', null, ['class' => 'form-control']) !!}
+        </div>
+        
+        <label for="email" class="mtop16">Password: </label>
+        <div class="input-group">
+            <div class="input-group-prepend">
+                <div class="input-group-text"><i class="fa-solid fa-lock"></i></div>
+            </div>
+            {!! Form::password('password', ['class' => 'form-control']) !!}
+        </div>
+        {!! Form::submit('Ingresar', ['class' => 'btn btn-success mtop16']) !!}
+        {!! Form::close() !!}
+    </div>
 @stop
