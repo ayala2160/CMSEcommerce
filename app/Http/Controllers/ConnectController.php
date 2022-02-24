f641fb9b2c8fd27e00eba04480de12d70f820ee7
Elida Ayala Guerrero <ayala2160@gmail.com>
2022-02-23 20:13:55 -0600

    Login de usuarios

--- app/Http/Controllers/ConnectController.php
+++ app/Http/Controllers/ConnectController.php
@@ -3,17 +3,48 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Validator, Hash;
+use Validator, Hash, Auth;
 use App\User;
 
 class ConnectController extends Controller
 {
+    public function __construct(){
+        $this->middleware('guest')->except(['getLogout']);
+    }
+
     public function getLogin(){
         return view('connect.login');
     }
+
+    public function postLogin(Request $request){
+        $rules = [
+            'email' => 'required|email',
+            'password' => 'required|min:8',
+        ];
+
+        $messages = [
+            'email.required' => 'Su correo electrónico es requerido.',
+            'email.email' => 'El formato de su correo electrónico es invalido.',
+            'password.required' => 'Por favor escriba una contraseña.',
+            'password.min' => 'La contraseña debe tener al menos 8 caracteres.',
+        ];
+                
+        $validator = Validator::make($request->all(), $rules, $messages);
+        if($validator->fails()):
+            return back()->withErrors($validator)->with('message','Se ha producido un error.')->with('typealert','danger');
+        else:
+            if(Auth::attempt(['email' => $request->input('email'), 'password' => $request->input('password')], true)):
+                return redirect('/');
+            else:
+                return back()->with('message','Correo electrónico o contraseña erronea.')->with('typealert','danger');
+            endif;
+        endif;
+    }
+    
     public function getRegister(){
         return view('connect.register');
     }
+    
     public function postRegister(Request $request){
         $rules = [
             'name' => 'required',
@@ -36,18 +67,23 @@ class ConnectController extends Controller
         ];
         
         $validator = Validator::make($request->all(), $rules, $messages);
-        if($validator->fails()):
-            return back()->withErrors($validator)->with('message','Se ha producido un error.')->with('typealert','danger');
-        else:
-            $user = new User;
-            $user->name = e($request->input('name'));
-            $user->lastname = e($request->input('lastname'));
-            $user->email = e($request->input('email'));
-            $user->password = Hash::make($request->input('password'));
+            if($validator->fails()):
+                return back()->withErrors($validator)->with('message','Se ha producido un error.')->with('typealert','danger');
+            else:
+                $user = new User;
+                $user->name = e($request->input('name'));
+                $user->lastname = e($request->input('lastname'));
+                $user->email = e($request->input('email'));
+                $user->password = Hash::make($request->input('password'));
 
-            if($user->save()){
-                return redirect('/login')->with('message','Su usuario se creo con exito, ahora puede iniciar sesión.')->with('typealert','success');
-            }
-        endif;
+                if($user->save()):
+                    return redirect('/login')->with('message','Su usuario se creo con exito, ahora puede iniciar sesión.')->with('typealert','success');
+                endif;
+            endif;
+        }
+
+    public function getLogout(){
+        Auth::logout();
+        return redirect('/');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,7 @@ Route::get('/', function () {
 });
 //Auth
 Route::get('/login', 'ConnectController@getLogin')->name('login');
+Route::post('/login', 'ConnectController@postLogin')->name('login');
 Route::get('/register', 'ConnectController@getRegister')->name('register');
 Route::post('/register', 'ConnectController@postRegister')->name('register');
+Route::get('/logout', 'ConnectController@getLogout')->name('login');
